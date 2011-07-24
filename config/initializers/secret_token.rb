3bd89c53e06266f3f1252201650548154e7eff4c
cocaineteddybear <alexander.didenko@gmail.com>
2011-07-24 16:07:13 +0400

    initial cmmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Factory::Application.config.secret_token = '884dacfbb3ff49fe46c955ed6b6eba280c4550ab13e4d4c3f2c876ebdc8ca9f945c878e9de5d010abb12c80ee39eccb5c38f458bdb76911f99777ff8ecdc1b05'
